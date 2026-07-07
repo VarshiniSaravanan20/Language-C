913e1ae30a1536692586379e107a0bbb9b151385
Varshini Saravanan <siva.varshinisaravanan@gmail.com>
2026-07-07 20:56:07 +0530

    Add files via upload

---
+++ problem11.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+   printf("%zu\n", sizeof(int));
+   printf("%zu\n", sizeof(float));
+   printf("%zu\n", sizeof(double));
+   printf("%zu", sizeof(char));
+   return 0;
+}
---
+++ problem19.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int i,n,sum=1;
+	printf("Integer N:");
+	scanf("%d",&n);
+	for(i=n;i>=1;i--){
+		sum=sum*i;
+	}
+	printf("%d",sum);
+	return 0;
+}
