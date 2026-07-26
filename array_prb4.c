c4f323259e2a0130de252d49fbcd6e36c7a35931
Varshini Saravanan <siva.varshinisaravanan@gmail.com>
2026-07-26 22:40:50 +0530

    Add files via upload

---
+++ array_prb4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[5], i;
+    int even = 0, odd = 0;
+
+    printf("Enter 5 numbers:\n");
+
+    for(i = 0; i < 5; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for(i = 0; i < 5; i++)
+    {
+        if(a[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    printf("Even = %d\n", even);
+    printf("Odd = %d\n", odd);
+
+    return 0;
+}
